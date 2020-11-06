cd34ce0c85750ca78c464bcf3427f87b3606f4e2
Caleb Zulawski <caleb.zulawski@gmail.com>
2020-11-06 10:39:30 -0500

    Extend platform support (#12)
    * Expand platform support
    
    * Add more systems to tests
    
    * Enable testing on pull requests

--- src/os/unix/posix/memfd.rs
+++ src/os/unix/posix/memfd.rs
@@ -20,35 +20,42 @@ pub fn memfd_open() -> Result<c_int> {
     }
 }
 
-#[cfg(not(any(target_os = "linux", target_os = "android")))]
+#[cfg(target_os = "freebsd")]
 pub fn memfd_open() -> Result<c_int> {
+    let fd = unsafe { libc::shm_open(libc::SHM_ANON, libc::O_RDWR, 0o600) };
+    if fd < 0 {
+        Err(Error::last_os_error(Operation::MemoryFd))
+    } else {
+        Ok(fd as c_int)
+    }
+}
+
+#[cfg(not(any(target_os = "linux", target_os = "android", target_os = "freebsd")))]
+pub fn memfd_open() -> Result<c_int> {
+    use libc::c_char;
     use rand::distributions::Alphanumeric;
     use rand::{thread_rng, Rng};
 
     const OFLAGS: c_int = libc::O_RDWR | libc::O_CREAT | libc::O_EXCL | libc::O_CLOEXEC;
 
-    // There *must* be a better way to do this...
-    let mut path: [i8; 18] = [
-        0x2f, 0x74, 0x6d, 0x70, 0x2f, 0x76, 0x6d, 0x61, 0x70, 0x2d, // "/tmp/vmap-"
-        0x58, 0x58, 0x58, 0x58, 0x58, 0x58, 0x58, 0x00, // "XXXXXXX\0"
-    ];
+    let mut path: [u8; 18] = *b"/tmp/vmap-XXXXXXX\0";
 
     let mut rng = thread_rng();
     let end = path.len() - 1;
 
     loop {
         for dst in &mut path[10..end] {
-            *dst = rng.sample(&Alphanumeric) as i8;
+            *dst = rng.sample(&Alphanumeric) as u8;
         }
 
-        let fd = unsafe { libc::shm_open(path.as_ptr(), OFLAGS, 0600) };
+        let fd = unsafe { libc::shm_open(path.as_ptr() as *const c_char, OFLAGS, 0o600) };
         if fd < 0 {
             let err = Error::last_os_error(Operation::MemoryFd);
             if err.raw_os_error() != Some(libc::EEXIST) {
                 return Err(err);
             }
         } else {
-            unsafe { libc::shm_unlink(path.as_ptr()) };
+            unsafe { libc::shm_unlink(path.as_ptr() as *const c_char) };
             return Ok(fd);
         }
     }
